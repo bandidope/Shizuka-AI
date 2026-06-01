38e1d5be2328bc1f1d58cf07da68107a65f2bda4
YALLICO OWNER <yallico2024@gmail.com>
2026-06-01 04:14:17 -0500

    Actualizar fun-marry.js

--- plugins/fun-marry.js
+++ plugins/fun-marry.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const marriagesFile = path.resolve('Tmp/marry.json');
+const marriagesFile = path.resolve('tmp/marry.json');
 let marriages = loadMarriages();
 let proposals = {}; 
 const confirmation = {};

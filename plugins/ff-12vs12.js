daf0f10294aeeef332d86acae38f649871ce7dba
YALLICO OWNER <yallico2024@gmail.com>
2026-06-01 00:09:59 -0500

    Actualizar ff-12vs12.js

--- plugins/ff-12vs12.js
+++ plugins/ff-12vs12.js
@@ -35,7 +35,7 @@ if (!args[0]) throw `
 ` 
 }
 handler.help = ['12vs12']
-handler.tags = ['ff']
+handler.tags = ['free']
 handler.command = /^(12vs12)$/i
 handler.group = false
 handler.admin = false
